2ba852e402688b3d2a47fc79402fc19d4872c9a7
Brendan McHugh <brendanmchugh12@gmail.com>
2019-09-27 22:56:21 +0200

    HighestScoringWord.js

--- HighestScoringWord.js
+++ HighestScoringWord.js
@@ -1,4 +1,15 @@
 
-exports.highestScoringWord = (word) => {
+exports.highestScoringWord = word => {
+  let words = word.match(/[A-Za-z]+/g);
+  let ans = [];
+  let reducer = (accum, char) => {
+    return accum + char.charCodeAt(0) - 96;
+  };
 
-  }
+  words.forEach(w => {
+    let chars = w.split("");
+    ans.push(chars.reduce(reducer, 0));
+  });
+
+  return words[ans.indexOf(Math.max(...ans))];
+};
